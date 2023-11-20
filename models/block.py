1d83d9a7bbf22ba6c0dd8c5bd1457c1219ced992
Le Thi Ngoc Anh <lethingocanh@Linas-MBP.AirPort>
2023-11-20 22:30:23 +0900

    split models

---
+++ models/block.py
@@ -0,0 +1,24 @@
+from hashlib import sha256
+import json
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        """
+        Constructor for the 'Block' class.
+        :param index:           Unique ID of the block.
+        :param transactions:    List of transactions.
+        :param timestamp:       Time of generation of the block.
+        :param previous_hash:   Hash of the previous block in the chain which this block is part of.
+        """
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+
+    def compute_hash(self):
+        """
+        A function that return the hash of the block contents.
+        """
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
--- node_server.py
+++ node_server.py
@@ -5,130 +5,9 @@ import time
 from flask import Flask, request
 import requests
 
+from models.block import Block
+from models.blockchain import Blockchain
 
-class Block:
-    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
-        """
-        Constructor for the 'Block' class.
-        :param index:           Unique ID of the block.
-        :param transactions:    List of transactions.
-        :param timestamp:       Time of generation of the block.
-        :param previous_hash:   Hash of the previous block in the chain which this block is part of.
-        """
-        self.index = index
-        self.transactions = transactions
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash
-        self.nonce = nonce
-
-    def compute_hash(self):
-        """
-        A function that return the hash of the block contents.
-        """
-        block_string = json.dumps(self.__dict__, sort_keys=True)
-        return sha256(block_string.encode()).hexdigest()
-    
-class Blockchain:
-    #difficulty of our PoW algorithm
-    difficulty = 2
-
-    def __init__(self):
-        """
-        Constructor for the 'Blockchain' class.
-        """
-        self.unconfirmed_transactions = []
-        self.chain = []
-
-    def create_genesis_block(self):
-        """
-        A function to generate genesis block and appends it to the chain. The block has index 0, previous_hash as 0, and a valid hash.
-        """
-        genesis_block = Block(0, [], 0, "0")
-        genesis_block.hash = genesis_block.compute_hash()
-        self.chain.append(genesis_block)
-    
-    @property
-    def last_block(self):
-        return self.chain[-1]
-    
-    def add_block(self, block, proof):
-        """
-        A function that adds the block to the chain after verification.
-        Verication include:
-        * Checking if the proof is valid.
-        * The previous_hash referred in the block and the hash of latest block in the chain match.
-        """
-        previous_hash = self.last_block.hash
-
-        if previous_hash != block.previous_hash:
-            return False
-        if not Blockchain.is_valid_proof(block, proof):
-            return False
-        
-        block.hash = proof
-        self.chain.append(block)
-        return True 
-    
-    @staticmethod
-    def proof_of_work(block):
-        """
-        Function that tries different values of nonce to get a hash that satisfies our difficulty criteria.
-        """
-        block.nonce = 0
-
-        computed_hash = block.computed_hash()
-        while not computed_hash.startwith('0' * Blockchain.difficulty):
-            block.nonce +=1
-            computed_hash = block.computed_hash()
-
-        return computed_hash
-    
-    def add_new_transacion(self, transaction):
-        self.unconfirmed_transactions.append(transaction)
-
-    @classmethod
-    def is_valid_proof(cls, block, block_hash):
-        """
-        Check if block_hash is valid hash of block and satisfies the difficulty criteria.
-        """
-
-        return (block_hash.startwith('0' * Blockchain.difficulty) and block_hash == block.compute_hash())
-    
-    @classmethod
-    def check_chain_validity(cls, chain):
-        result = True
-        previous_hash = "0"
-
-        for block in chain:
-            block_hash = block.hash
-            #remove the hash field to recompute the hash again
-            #using 'compute_hash' method.
-            delattr(block, "hash")
-
-            if not cls.is_valid_proof(block, block_hash) or previous_hash != block.previous_hash:
-                result = False
-                break
-            
-            block.hash, previous_hash = block_hash, block_hash
-        return result
-    def mine(self):
-        """
-        This function serves as an interface to add the pending transactions to the blockchain by adding them to the block and figuring out Proof of Work.
-        """
-        if not self.unconfirmed_transactions:
-            return False
-        
-        last_block = self.last_block
-
-        new_block = Block(index=last_block.indext + 1, transactions=self.unconfirmed_transactions, timestamp=time.time(), previous_hash=last_block.hash)
-
-        proof = self.proof_of_work(new_block)
-        self.add_block(new_block, proof)
-        self.add_block(new_block, proof)
-        self.unconfirmed_transactions = []
-
-        return True
-    
 # Initialize flask application
 app = Flask(__name__)
 
@@ -141,7 +20,7 @@ peers = set()
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Worldsisaodjasdokads!</p>"
+    return "<p>Hello, Blockchain!</p>"
 
 # endpoint to submit a new transaction. This will be used by our application to add new data (posts) to the blockchain
 @app.route('/new_transaction', methods=['POST'])
@@ -221,7 +100,7 @@ def verify_and_add_block():
                     block_data["timestamp"],
                     block_data["previous_hash"],
                     block_data["nonce"])
-    proof = block_data['hash']
+    proof = block.compute_hash()
     added = blockchain.add_block(block, proof)
     if not added:
         return "The block was discarded by the node", 400
